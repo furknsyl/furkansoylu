7dc947851c18bb98870ef6365fe95fc2f710146a
furknsyl <fsoylu500@gmail.com>
2025-06-22 18:42:49 +0300

    Theme icon fixed

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -38,6 +38,7 @@ export const ThemeToggle = () => {
         "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outlin-hidden"
       )}
+
     >
       {isDarkMode ? (
         <Sun className="h-6 w-6 text-yellow-300" />
