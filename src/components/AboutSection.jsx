14bfa387c2d6fd269fd9175cba715a85b3ad8da0
furknsyl <fsoylu500@gmail.com>
2025-09-11 13:17:26 +0300

    Bazı hatalar giderildi

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/FurkanSoyluCV.pdf"
+                href="/FurkanSOYLUCV.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
